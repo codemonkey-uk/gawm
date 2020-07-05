0a61d9f42863e36e1d6e9cff197706aaeeb25e04
codemonkey-uk <codemonkey.uk@gmail.com>
2020-07-05 11:22:26 +0100

    fixed caching, js tidy up

--- api/new.php
+++ api/new.php
@@ -12,6 +12,10 @@ $encoded = json_encode($data);
 //temp, return to caller
 // todo: write to DB, return UID
 header('Content-Type: application/json');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 echo $encoded;
 
 ?>
