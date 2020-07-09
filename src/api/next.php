a084988382d6d0187c67a6f964b4684f7dc32347
codemonkey-uk <codemonkey.uk@gmail.com>
2020-07-09 22:00:56 +0100

    next scene now reads from and writes back to the db

--- src/api/next.php
+++ src/api/next.php
@@ -1,14 +1,27 @@
 <?php
 require 'gawm.php';
+require 'db.php';
 
 // Takes raw data from the request
 $json = file_get_contents('php://input');
 
 // Converts it into a PHP object
-$data = json_decode($json, true);
+$request = json_decode($json, true);
 
-// execute any end-scene mechanics, advance to next scene/act
-end_scene($data);
+$game_id = $request["game_id"];
+
+$data = null;
+$link = load_for_edit($game_id, $data);
+
+try {
+    // execute any end-scene mechanics, advance to next scene/act
+    end_scene($data);
+    complete_edit($link, $game_id, $data);
+} catch (Exception $e) {
+    http_response_code(400);
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+    return;
+}
 
 // convert back to json
 $encoded = json_encode($data);
