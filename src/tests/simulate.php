1df784bbeb29993fe69d9798ad33431ab8b12cf9
codemonkey-uk <codemonkey.uk@gmail.com>
2020-08-10 13:39:26 +0100

    simulation for the chart shared in discord

--- src/tests/simulate.php
+++ src/tests/simulate.php
@@ -2,19 +2,6 @@
 require_once '../api/gawm.php';
 require_once 'testing.php';
 require_once 'playthrough.php';
-
-    //$other_players = array_filter( $player_ids, 
-    //    function($id)use($data){return $id!=$data["most_innocent"];}
-    //);
-
-
-    // accuse who has the most guilty tokens
-    // $g = gawm_list_players_by_most_guilty_tokens($data);
-    // $g = gawm_list_players_by_net_tokens($data);
-    // $g = gawm_list_players_by_guilty_tokens_vs_innocence_scores($data, $rules);
-    // if ($g[0]==$data["most_innocent"])
-    //     array_shift($g);
-    // $accused = $g[0];
     
 $most_guilty_tokens = function ($data)
 {
@@ -43,36 +30,58 @@ try{
 
     $scenario[] = gawm_default_rules;
     $scenario[] = make_rules(["new_player_tokens" => range(1,4)]);
+    $guilt_bias = [75,50,25];
     
-    foreach($scenario as $rules)
-    {
-        $fates=[
+    $ft=[
             "got_caught" => 0,
             "gawm" => 0,
             "got_it_right" => 0,
             "got_it_wrong" => 0,
             "got_framed" => 0,
             "got_out_alive" => 0
-        ];    
+        ];
+        
+    echo "guilt, innocence, ";    
+    foreach(array_keys($ft) as $h)
+        echo $h.', ';
+    echo "rules\n";
+    
+    foreach($guilt_bias as $bias)
+    foreach($scenario as $rules)
+    {
+        $fates=$ft;
+        $tc=['guilt' => 0,'innocence' => 0];
+        
+        // random pick of strategies
+        $f[] = $random_accusation;
+        $f[] = $most_guilty_tokens;
+        $f[] = $net_tokens;
+        $f[] = function($data) use ($rules) {
+            $g = gawm_list_players_by_guilty_tokens_vs_innocence_scores($data, $rules);
+            if ($g[0]==$data["most_innocent"])
+                array_shift($g);
+            return $g[0];
+        };
+        
         for ($i = 1; $i <= 500; $i++)
         {
             for ($c = 4; $c <= 6; $c++)
             {
-                $f = function($data) use ($rules) {
-                    $g = gawm_list_players_by_guilty_tokens_vs_innocence_scores($data, $rules);
-                    if ($g[0]==$data["most_innocent"])
-                        array_shift($g);
-                    return $g[0];
-                };
-                
-                test_playthrough($c, $rules, $f);
+                test_playthrough($c, $rules, $f[array_rand($f)], $bias);
                 foreach(array_keys($data["players"]) as $pk)
                 {
                     $fates[$data["players"][$pk]["fate"]]++;
                 }
+                foreach($tc as $k => &$v)
+                    $v += token_count($data, $k);
             }
         }
-        echo json_encode($fates) . "\n";
+
+        echo $tc['guilt'] . ', ' . $tc['innocence']. ', ';
+        foreach($fates as $v)
+            echo $v.', ';
+        echo '"'.json_encode($rules["new_player_tokens"]).'"'."\n";
+
     }
 
 }
