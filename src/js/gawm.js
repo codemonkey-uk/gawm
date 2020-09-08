7faafb34b2aa5e25e85c7a12924b68fb62fc45d7
Thad <codemonkey-uk@users.noreply.github.com>
2020-09-08 19:30:34 +0100

    Css refactor (#101)
    * duplicated style from html moved to css
    * sorted out div button and a href use
    * actions -> cardmenu, to differentiate for landing page call-to-action div, boosted menu font size slightly
    * player and hand styles moved to gawm.css, gawm.css reorganised and documented
    * css brace and tab style consistency
    * cleaned up snackbar css and extended message time on screen
    * pointer and token styles merged into components.css

--- src/js/gawm.js
+++ src/js/gawm.js
@@ -64,12 +64,12 @@ function deck_order(deck)
 }
 
 var card_template = `<div class="halfcard _TYPE">
-  <div class="header" style="_CURSOR" onclick="toggle_show(this.parentElement,'actions')">
+  <div class="header" style="_CURSOR" onclick="toggle_show(this.parentElement,'cardmenu')">
   <div class="title">_TYPE_TXT</div>
   <div class="subtitle">_SUBTYPE</div>  
   </div>
   <div class="name" onclick="toggle_show(this.parentElement, 'flavour')"><p>_NAME</p></div>
-  <div class="actions">_ACTIONS</div>
+  <div class="cardmenu">_ACTIONS</div>
   <div class="flavour">
   <p id='flavour-_ID_editp' contenteditable="true" onblur="saveNote('flavour-_ID_editp','_TYPE','_ID')">_DESC</p>
   </div>
@@ -192,7 +192,7 @@ function is_hand_redacted(hand)
 
 function token_html(type, i, click)
 {
-    var result = "<div class='"+type+"'";
+    var result = "<div class='token "+type+"'";
     if (click)
     {
         result += "onclick='"+click+"'";
@@ -497,7 +497,7 @@ function show_hand(player,player_uid)
 }
 
 var accused_template =`
-<div class="pointer"><div class="frame">
+<div class="token pointer"><div class="frame">
 <img style="margin-top: 0px; margin-bottom: 1px;" src="assets/accuse.svg" alt="accuse graphic"/>
 <p>ACCUSED</p>
 </div></div>
@@ -509,7 +509,7 @@ function accused_html()
 }
 
 var pointer1_template =`
-<div class="pointer" style="cursor: pointer" onclick="_ACTION">
+<div class="token pointer" style="cursor: pointer" onclick="_ACTION">
 <div class="frame">
 <img src="assets/arrow.svg" alt="arrow icon"/>
 <p>_TEXT</p></div>
@@ -524,11 +524,11 @@ function pointer1_html(text, action)
 }
 
 var pointer_template =`
-<div class="pointer" style="_CURSOR" onclick="toggle_show(this, 'actions')">
+<div class="token pointer" style="_CURSOR" onclick="toggle_show(this, 'cardmenu')">
 <div class="frame">
 <img src="assets/arrow.svg" alt="arrow icon"/>
 <p>_TEXT</p></div>
-<div class="actions">_ACTIONS</div>
+<div class="cardmenu">_ACTIONS</div>
 </div>
 `;
 
@@ -542,11 +542,11 @@ function pointer_html(text, menu)
 }
 
 var tokenback_template = `
-<div class='_TYPE' style="_CURSOR" onclick="toggle_show(this, 'actions')">
+<div class='token _TYPE' style="_CURSOR" onclick="toggle_show(this, 'cardmenu')">
 <div class="frame">
 <img src="_IMGURL" alt="_ALT"><p>_TYPE</p>
 </div>
-<div class="actions">_ACTIONS</div>
+<div class="cardmenu">_ACTIONS</div>
 </div>
 `;
 
@@ -1259,8 +1259,8 @@ function error_popup(msg)
         x.className = "show";
         x.innerHTML = msg
 
-        // After 3 seconds, remove the show class from DIV
-        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+        // After 5 seconds, remove the show class from DIV
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5500);
     }
 }
 
